d0de5a853d9ba6cf36260448ef5f5934fbdf250c
sebas2426 <tobiaspalma505@gmail.com>
2024-11-28 11:36:40 -0300

    Creación del reporte para profesores

--- backend/controlers/authController.js
+++ backend/controlers/authController.js
@@ -52,7 +52,7 @@ exports.login = async (req, res) => {
                     });
                 } else {
                     // Registro OK
-                    conexion.query('INSERT INTO users (username, name, user_pass) VALUES ($1, $2, $3)', [user, name, passHash], (error, results) => {
+                    conexion.query('INSERT INTO users (username, name, user_pass, tipo_usuario) VALUES ($1, $2, $3, A)', [user, name, passHash], (error, results) => {
                         if (error) {
                             console.error(error);
                             res.render('login', {
